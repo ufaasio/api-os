b8c68590abf2309e6c33ce1bdee7aa0a108c2085
Mahdi Kiani <mahdikiany@gmail.com>
2025-01-21 15:56:04 +0000

    api-os

--- app/server/server.py
+++ app/server/server.py
@@ -4,10 +4,7 @@ from fastapi_mongo_base.core import app_factory
 
 from . import config
 
-app = app_factory.create_app(
-    settings=config.Settings(),
-    ufaas_handler=False,
-)
+app = app_factory.create_app(settings=config.Settings())
 
 app.add_middleware(DynamicCORSMiddleware)
 app.include_router(apps_router, prefix=f"{config.Settings.base_path}")
